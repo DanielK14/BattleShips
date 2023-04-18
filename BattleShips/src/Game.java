10be7a98bc55e22a36dcc6bb7e84d490e8b1ef3a
Daniel Krois <danielkrois@hotmail.com>
2023-04-18 20:30:05 +0200

    Fixed a bug that allowed player to hit same number multiple times

--- BattleShips/src/Game.java
+++ BattleShips/src/Game.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+
 public class Game {
 	public static void main(String[] args) {
 		int numOfGuesses = 0;
 		GameHelper helper = new GameHelper();
-		SimpleDotCom theDotCom = new SimpleDotCom();
+		DotCom theDotCom = new DotCom();
 		int randomNum = (int) (Math.random()*5);
 		
-		int[] locations = {randomNum, randomNum+1, randomNum+2};
+		ArrayList<String> locations = new ArrayList<String>();
+		locations.add(String.valueOf(randomNum));
+		locations.add(String.valueOf(randomNum + 1));
+		locations.add(String.valueOf(randomNum + 2));
 		
 		theDotCom.setLocationCells(locations);
 		boolean isAlive = true;
